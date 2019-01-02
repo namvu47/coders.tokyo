48139c34496079507ee4c955ff2756f19ae4e689
Nam Vu <namvu47@gmail.com>
2019-01-02 01:19:53 -0800

    3-Query Parameters

---
+++ ExpressJS/3 - Query Parameters/index.js
@@ -0,0 +1,31 @@
+const express = require ('express');
+const app = express();
+const port = 3000;
+
+app.set ('view engine', 'pug');
+app.set('views', './views');
+
+var users = [
+		{ id: 1, name: "Mint" },
+		{ id: 2, name: "Big" }
+	];
+
+app.get ('/', (req, res) => res.render('index',{
+	name: "Mint"
+}));
+
+app.get ('/users', (req, res) => res.render('users/index', {
+	users: users
+}));
+
+app.get ('users/search', function (req, res) {
+	var q = req.query.q;
+	matchedResult = users.filter( 
+		user => user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1
+	);
+	res.render('/users/index', {
+		users: matchedResult
+	});
+});
+
+app.listen (port, () => console.log('Server listening at port: ' + port));
