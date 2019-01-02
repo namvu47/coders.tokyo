6d04a753faedf34ca6a58626911935654d1239ce
Nam Vu <namvu47@gmail.com>
2019-01-02 08:42:04 +0700

    ExpressJS - Lesson 2

---
+++ ExpressJS/2 - Template Engines/index.js
@@ -0,0 +1,20 @@
+const express = require ('express');
+const app = express();
+const port = 3000;
+
+app.set ('view engine', 'pug');
+app.set('views', './views');
+
+
+app.get ('/', (req, res) => res.render('index',{
+	name: "Mint"
+}));
+
+app.get ('/users', (req, res) => res.render('users/index', {
+	users: [
+		{ id: 1, name: "Mint" },
+		{ id: 2, name: "Big" }
+	]
+}));
+
+app.listen (port, () => console.log('Server listening at port: ' + port));
