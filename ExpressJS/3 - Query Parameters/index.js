d1de2acef663a995e67e2509f595210fbdc59816
Nam Vu <namvu47@gmail.com>
2019-01-02 21:27:33 -0800

    ExpressJS - Lesson 3 fixed

--- ExpressJS/3 - Query Parameters/index.js
+++ ExpressJS/3 - Query Parameters/index.js
@@ -2,30 +2,32 @@ const express = require ('express');
 const app = express();
 const port = 3000;
 
-app.set ('view engine', 'pug');
+app.set('view engine', 'pug');
 app.set('views', './views');
 
 var users = [
-		{ id: 1, name: "Mint" },
-		{ id: 2, name: "Big" }
-	];
+	{ id: 1, name: "Mint" },
+	{ id: 2, name: "Big" }
+];
 
-app.get ('/', (req, res) => res.render('index',{
+app.get('/', (req, res) => res.render('index',{
 	name: "Mint"
 }));
 
-app.get ('/users', (req, res) => res.render('users/index', {
+app.get('/users', (req, res) => res.render('users/index', {
 	users: users
 }));
 
-app.get ('users/search', function (req, res) {
+app.get('/users/search', function (req, res) {
 	var q = req.query.q;
-	matchedResult = users.filter( 
-		user => user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1
-	);
-	res.render('/users/index', {
-		users: matchedResult
+	var matchedUser = users.filter(function (user) {
+		return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
 	});
+
+	res.render('users/index', {
+		users: matchedUser
+	});
+
 });
 
 app.listen (port, () => console.log('Server listening at port: ' + port));
