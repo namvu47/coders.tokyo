78454e179fc385818b7d30cdca1601c390c55fe9
Nam Vu <namvu47@gmail.com>
2019-01-16 01:01:50 -0800

    12. Server Side Validation

---
+++ ExpressJS/12. serverSideValidation/index.js
@@ -0,0 +1,27 @@
+// MVC: controllers - contain all func from routes
+
+const express = require ('express');
+const bodyParser = require('body-parser');
+
+const userRoutes = require('./routes/user.route');
+
+const app = express();
+
+const port = 3000;
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.render('index',{
+	name: "Mint"
+}));
+
+app.use('/users', userRoutes);
+
+
+app.listen (port, () => console.log('Server listening at port: ' + port));
