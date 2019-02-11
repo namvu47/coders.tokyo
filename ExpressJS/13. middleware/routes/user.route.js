367ab4e98d35c3a49b350973a1587e45c51e51b3
Nam Vu <namvu47@gmail.com>
2019-02-11 23:31:21 +0700

    13. Middleware

---
+++ ExpressJS/13. middleware/routes/user.route.js
@@ -0,0 +1,20 @@
+// divide index.js to different components
+
+const express = require('express');
+
+const controller = require('../controllers/user.controller')
+const validate = require('../validate/user.validate')
+
+const router = express.Router();
+
+router.get('/', controller.index);
+
+router.get('/search', controller.search);
+
+router.get('/create', controller.create);
+
+router.get('/:id', controller.get);
+
+router.post('/create', validate.postCreate,controller.postCreate);
+
+module.exports = router;
