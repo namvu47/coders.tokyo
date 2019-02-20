dcbe11395845c7e652104751e406f55585ed1979
Nam Vu <namvu47@gmail.com>
2019-02-20 18:07:58 +0700

    22 - CSRF attack

---
+++ ExpressJS/22. CSRF attack/middleware/auth.middleware.js
@@ -0,0 +1,14 @@
+const db = require('../lowdb');
+
+module.exports.requireAuth = function (req, res, next) {
+	var user = db.get('users').find({ id: req.signedCookies.userId }).value();
+
+	if ((!req.signedCookies.userId) || (!user)) {
+		res.redirect('/auth/login');
+		return;
+	};
+
+	res.locals.user = user;
+
+	next();
+};
---
+++ ExpressJS/22. CSRF attack/routes/user.route.js
@@ -0,0 +1,28 @@
+// divide index.js to different components
+
+const express = require('express');
+const multer = require('multer');
+
+const controller = require('../controllers/user.controller');
+const validate = require('../validate/user.validate');
+const authMiddleware = require('../middleware/auth.middleware');
+
+const upload = multer({ dest: './public/uploads/' })
+
+const router = express.Router();
+
+router.get('/', controller.index);
+
+router.get('/search', controller.search);
+
+router.get('/create', controller.create);
+
+router.get('/:id', controller.get);
+
+router.post('/create',
+	upload.single('avatar'), 
+	validate.postCreate,
+	controller.postCreate
+);
+
+module.exports = router;
