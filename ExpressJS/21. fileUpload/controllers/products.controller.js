d739bd1e16478467c7596f2008e386609775eeab
Nam Vu <namvu47@gmail.com>
2019-02-20 13:27:24 +0700

    21- File upload, multer middleware

---
+++ ExpressJS/21. fileUpload/controllers/products.controller.js
@@ -0,0 +1,15 @@
+const db = require('../lowdb');
+const shortid = require('shortid');
+
+module.exports.index = function (req, res) {
+	var page = parseInt(req.query.page) || 1;
+	var perPage = 8;
+
+	var begin = (page-1)*perPage;
+	var end = page*perPage;
+
+
+	res.render('products/index', {
+		products: db.get('products').value().slice(begin,end)
+	})
+};
---
+++ ExpressJS/21. fileUpload/index.js
@@ -0,0 +1,37 @@
+// multer module: read form data, encrypt = multipart
+// bodyparser: only read data encrypt = urlencoded (text), cannot upload file
+
+require('dotenv').config(); 
+
+const express = require ('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser')
+
+const userRoutes = require('./routes/user.route');
+const authRoutes = require('./routes/auth.route');
+const productsRoutes = require('./routes/products.route');
+
+const authMiddleware = require('./middleware/auth.middleware');
+
+const app = express();
+
+const port = 3000;
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(cookieParser(process.env.SESSON_SECRET));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.render('index',{
+	name: "Mint"
+}));
+
+app.use('/users', authMiddleware.requireAuth, userRoutes);
+app.use('/auth', authRoutes);
+app.use('/products', productsRoutes);
+
+app.listen (port, () => console.log('Server listening at port: ' + port));
